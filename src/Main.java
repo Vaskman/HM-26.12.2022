cb9bd44cfbbac918120477ce8cd2014ef4667812
Vaskman <z.vaskman@gmail.com>
2022-12-26 14:57:36 +0300

    1.2.3 задачи (готовы)

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,37 @@ public class Main {
         System.out.println("Задача 1");
         var dog = 8.0;
         System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        System.out.println("Задача 3");
+        dog = dog -3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+
+
 
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-        var cat =3.6;
-        System.out.println(cat);
+
+
         }
     public static void task3(){
         System.out.println("Задача 3");
-        var paper = 763789;
-        System.out.println(paper);
+
     }
 }
